7e46a9ea0f71d31092441d2f1079b4fa0e792fc0
Kadija T <hmani.kadija11@gmail.com>
2024-08-14 11:16:39 +0200

    enabling resizing and cropping with sharp

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,109 +1,36 @@
-// const sharp = require('sharp');
-// const path = require('path');
-// const fs = require('fs');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
-// const resizeAndCropImage = async (req, res, next) => {
-//   if (!req.file) {
-//     return next();
-//   }
+async function resizeAndCropImage(req, res, next) {
+  if (req.file) {
+    const imagePath = req.file.path;
 
-//   const imagePath = req.file.path;
-//   const originalFilename = req.file.filename.split('.')[0];
-//   const outputFilename = `${originalFilename}-${Date.now()}.jpeg`;
-//   const outputPath = path.join(path.dirname(imagePath), outputFilename);
+    try {
+      const resizedImage = await sharp(imagePath)
+        .resize(350, 260)
+        .extract({ left: 0, top: 0, width: 350, height: 260 })
+        .jpeg({ quality: 80 })
+        .toBuffer();
 
-//   try {
-//     await sharp(imagePath)
-//       .resize(350, 260)
-//       .extract({ left: 0, top: 0, width: 350, height: 260 })
-//       .toFile(outputPath);
+      const originalName = req.file.originalname;
+      const imageExtension = path.extname(originalName);
+      const imageFilename = `${originalName.split('.')[0].split(' ').join('_')}_${Date.now()}${imageExtension}`;
+      const newImagePath = path.join(path.dirname(imagePath), imageFilename);
 
-//     fs.unlink(imagePath, (err) => {
-//       if (err) {
-//         console.error('Erreur lors de la suppression de l\'image d\'origine:', err);
-//         return res.status(500).send('Erreur lors de la suppression de l\'image d\'origine');
-//       }
+      await fs.promises.writeFile(newImagePath, resizedImage);
 
-//       req.file.filename = outputFilename;
-//       req.file.path = outputPath;
+      req.file.filename = imageFilename; 
+      req.file.path = newImagePath;
 
-//       next();
-//     });
-//   } catch (error) {
-//     console.error(error.message);
-//     return res.status(500).send();
-//   }
-// };
+      next();
+    } catch (error) {
+      console.error('Erreur lors du traitement de l\'image:', error);
+      return res.status(500).send('Erreur lors du traitement de l\'image');
+    }
+  } else {
+    next();
+  }
+}
 
-// module.exports = resizeAndCropImage;
-
-
-
-
-
-
-
-
-
-
-// const sharp = require('sharp');
-// const path = require('path');
-// const fs = require('fs');
-
-// const resizeAndCropImage = async (req, res, next) => {
-//   if (!req.file) {
-//     return next(); 
-//   }
-
-//   const imagePath = req.file.path; 
-//   const outputFilename = `image-${Date.now()}.jpeg`; 
-//   const outputPath = path.join(__dirname, '..', 'images', outputFilename); 
-
-//   try {
-//     await sharp(imagePath)
-//       .resize(150, 97)
-//       .toFile(outputPath);
-
-//     fs.unlink(imagePath, (err) => {
-//       if (err) {
-//         console.error('Erreur lors de la suppression de l\'image d\'origine:', err);
-//         return res.status(500).send('Erreur lors de la suppression de l\'image d\'origine');
-//       }
-
-//       req.file.filename = outputFilename;
-//       req.file.path = outputPath; 
-//       next(); 
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).send('Erreur lors du traitement de l\'image');
-//   }
-// };
-
-// module.exports = resizeAndCropImage;
-
-/*** */
-
-// const sharp = require('sharp')
-
-// const resizeImage = () => {
-//   const resize = sharp('./images/robo.jpg')
-//   .resize(350, 260)
-//   .toFile(__dirname + '/processed_images/resize_robo.jpg')
-
-//   console.log(resize)
-// }
-
-// resizeImage()
-
-// const sharp = require('sharp')
-
-// const cropImage = () => {
-//   sharp('./images/robo.jpg')
-//   .extract({left: 740, width: 500, height: 300, top: 340})
-//   .toFile(__dirname + '/processed_images/crop_robo.png')
-// }
-
-// cropImage()
-
-/***** */
+module.exports = resizeAndCropImage;
--- routes/books.js
+++ routes/books.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-//const resizeAndCropImage = require('../middleware/sharp-config');
+const resizeAndCropImage = require('../middleware/sharp-config');
 
 const booksCtrl = require('../controllers/books');
 
-router.post('/', auth, multer, booksCtrl.createBook);
+router.post('/', auth, multer, resizeAndCropImage, booksCtrl.createBook);
 router.get('/', booksCtrl.getAllBooks);
 router.get('/bestrating', booksCtrl.getBestRating);
 router.get('/:id', booksCtrl.getOneBook);
 
-router.put('/:id', auth, multer,  booksCtrl.modifyBook);
+router.put('/:id', auth, multer, resizeAndCropImage, booksCtrl.modifyBook);
 router.post('/:id/rating', auth, booksCtrl.rateAndGetAverageRating);
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
