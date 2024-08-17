0a37340b6d9befd5533515c762ea25a7c630d5d4
Kadija T <hmani.kadija11@gmail.com>
2024-08-17 17:01:11 +0200

    cleaning

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,31 +1,30 @@
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs');
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 async function resizeAndCropImage(req, res, next) {
   if (req.file) {
     const imagePath = req.file.path;
 
     try {
-     
       sharp.cache(false);
 
-      
       const resizedImage = await sharp(imagePath)
         .resize(350, 260)
         .extract({ left: 0, top: 0, width: 350, height: 260 })
-        .jpeg({ quality: 80 }) 
+        .jpeg({ quality: 80 })
         .toBuffer();
 
-      const originalName = req.file.originalname.split('.')[0].split(' ').join('_');
+      const originalName = req.file.originalname
+        .split(".")[0]
+        .split(" ")
+        .join("_");
       const extension = path.extname(req.file.filename);
       const newFilename = `${originalName}_${Date.now()}${extension}`;
       const newImagePath = path.join(path.dirname(imagePath), newFilename);
 
-      
       await fs.promises.writeFile(newImagePath, resizedImage);
 
-     
       await fs.promises.unlink(imagePath);
 
       req.file.filename = newFilename;
@@ -33,8 +32,8 @@ async function resizeAndCropImage(req, res, next) {
 
       next();
     } catch (error) {
-      console.error('Erreur lors du traitement de l\'image:', error);
-      return res.status(500).send('Erreur lors du traitement de l\'image');
+      console.error("Erreur lors du traitement de l'image:", error);
+      return res.status(500).send("Erreur lors du traitement de l'image");
     }
   } else {
     next();
